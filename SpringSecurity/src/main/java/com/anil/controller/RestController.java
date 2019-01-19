0aa35d98d9f09a9880a18c85e58b5cd776f99ca8
BDII1335 <BDII1335@BDII1335.BDII.COM>
2019-01-19 13:20:40 +0530

    @pathvariable and @postConstruct

--- SpringSecurity/src/main/java/com/anil/controller/RestController.java
+++ SpringSecurity/src/main/java/com/anil/controller/RestController.java
@@ -3,23 +3,35 @@ package com.anil.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.anil.model.Employee;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-	@GetMapping("/hr/emp")
-	public List<Employee> emplist()
+	List<Employee> emp = new ArrayList<Employee>();
+	@PostConstruct
+	public void storedata()
 	{
-		List<Employee> emp = new ArrayList<Employee>();
 		emp.add(new Employee(1, "anil", "java"));
 		emp.add(new Employee(2, "suni", "python"));
 		emp.add(new Employee(3, "sajid", "java"));
 		emp.add(new Employee(4, "nakul", "java"));
 		emp.add(new Employee(5, "ankue", "php"));
-		return emp;
+			
+	}
+	
+	
+	@GetMapping("/hr/emp/{eid}")
+	public Employee emplist(@PathVariable int eid)
+	{
+		eid--;
+		Employee obj  =emp.get(eid);
+		return obj;
 		
 	}
 }
